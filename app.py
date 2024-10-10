b1524411a1f7a7c83e0c6d3737a71e71006a05a4
edwin <elshopojr@gmail.com>
2024-10-10 14:52:34 -0400

    se cambio

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template, request
+from flask_frozen import Freezer
 
 app = Flask(__name__)
+freezer = Freezer(app)
 
-# Página de inicio
+# Página principal
 @app.route('/')
-def home():
+def index():
     return render_template('index.html')
 
 # Página Quiénes Somos
@@ -22,16 +24,17 @@ def services():
 def news():
     return render_template('news.html')
 
-# Página de Contacto
+# Página de Contacto con manejo de método POST
 @app.route('/contact', methods=['GET', 'POST'])
 def contact():
     if request.method == 'POST':
-        nombre = request.form['nombre']
-        email = request.form['email']
-        mensaje = request.form['mensaje']
-        # Aquí puedes procesar los datos como enviarlos a un email o guardarlos
+        nombre = request.form.get('nombre')
+        email = request.form.get('email')
+        mensaje = request.form.get('mensaje')
+        # Aquí puedes procesar los datos, como enviarlos a un email o guardarlos
         return f"Mensaje enviado por {nombre}."
     return render_template('contact.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True)  # Puedes usar esto para desarrollo
+    # freezer.freeze()  # Usa esto para congelar la aplicación para producción
